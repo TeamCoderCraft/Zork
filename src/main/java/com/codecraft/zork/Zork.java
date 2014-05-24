33f3333f93c97581588e53fce91c6dd914472593
Matthieu Parizeau <mattparizeau@hotmail.com>
2014-05-24 09:20:13 -0400

    Initial Commit

---
+++ src/main/java/com/codecraft/zork/Zork.java
@@ -0,0 +1,18 @@
+package com.codecraft.zork;
+
+import com.codecraft.zork.references.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+public class Zork {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+}
---
+++ src/main/java/com/codecraft/zork/references/Reference.java
@@ -0,0 +1,7 @@
+package com.codecraft.zork.references;
+
+public class Reference {
+	public static final String MOD_ID = "cczork";
+	public static final String MOD_NAME = "Zork";
+	public static final String MOD_VERSION = "${version}";
+}
